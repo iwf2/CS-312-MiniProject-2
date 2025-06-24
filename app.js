28f2f0e334d4be6555005a30853d34aeb01c3ae7
Isaac Faulkner <ifaulkner2020@gmail.com>
2025-06-23 17:52:46 -0700

    update

--- app.js
+++ app.js
@@ -11,22 +11,31 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//grab ejs index
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+//post joke after submit to after grabbing from api via url
 app.post('/joke', async (req, res) => {
   const { name, category } = req.body;
   const url = `https://v2.jokeapi.dev/joke/${category}?blacklistFlags=nsfw`;
 
-  try {
+  //capture result 
+  try 
+  {
     const response = await axios.get(url);
     const jokeData = response.data;
     res.render('result', { name, jokeData });
-  } catch (error) {
+  } 
+  
+  //if there is error report error message
+  catch (error) 
+  {
     res.render('error', { message: 'Failed to fetch a joke. Try again!' });
   }
 });
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
